ddae5a0e795762eefdb0f21e327378c685141bc8
YoWuwuuuw <2216348784@qq.com>
2025-06-06 14:39:51 +0800

    opt

--- core/src/main/java/org/apache/seata/core/rpc/netty/http/HttpDispatchHandler.java
+++ core/src/main/java/org/apache/seata/core/rpc/netty/http/HttpDispatchHandler.java
@@ -54,7 +54,7 @@ public class HttpDispatchHandler extends SimpleChannelInboundHandler<HttpRequest
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     /**
-     * HTTP request processing thread pool, independent of Netty IO threads, to avoid blocking network processing
+     * HTTP request processing thread pool, independent of Netty IO threads, to avoid blocking network processing.
      */
     private static final ExecutorService httpHandlerThreads = new ThreadPoolExecutor(
             NettyServerConfig.getMinHttpPoolSize(),
